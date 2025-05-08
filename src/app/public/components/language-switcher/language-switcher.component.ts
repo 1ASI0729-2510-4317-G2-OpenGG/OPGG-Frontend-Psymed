af9bad5bcd0b68becda6f5dc57e1314ed74def19
Sihuar Ccotarma <Sihuarc1711@gmail.com>
2025-05-08 17:32:09 -0500

    chore: Add Language switcher component

---
+++ src/app/public/components/language-switcher/language-switcher.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {MatButtonToggle} from '@angular/material/button-toggle';
+
+@Component({
+  selector: 'app-language-switcher',
+  imports: [
+    MatButtonToggle
+  ],
+  templateUrl: './language-switcher.component.html',
+  styleUrl: './language-switcher.component.css'
+})
+export class LanguageSwitcherComponent {
+  currentLang: string;
+  languages = ['en', 'es'];
+
+  constructor(private translate: TranslateService) {
+    this.currentLang = this.translate.currentLang;
+  }
+
+  useLanguage(language: string) {
+    this.translate.use(language);
+  }
+}
